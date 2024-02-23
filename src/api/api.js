96aa433be1d2503ef10fa0fed273a9ff83ae4885
Itminan30 <itminan30@gmail.com>
2024-02-23 12:14:20 +0600

    Added some final touches

--- src/api/api.js
+++ src/api/api.js
@@ -31,32 +31,4 @@ api.interceptors.response.use(
     }
 )
 
-// const token = "jhdjfhdjfhdjfh";
-
-// // request interceptor
-// api.interceptors.request.use(
-//     (config) => {
-//         config.headers["Authorization"] = "Bearer " + token;
-//         return config;
-//     },
-//     (error) => {
-//         return Promise.reject(error);
-//     }
-// );
-
-// // response interceptor
-// api.interceptors.response.use(
-//     (response) => {
-//         return response;
-//     },
-//     (err) => {
-//         if (err.response) {
-//             // error came from server
-//             err.message = `Error from server: status: ${err.response.status} - message: ${err.response.statusText}`;
-//         }
-
-//         return Promise.reject(err);
-//     }
-// );
-
 export default api;
